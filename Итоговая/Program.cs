9f239ec0eb871af941a931ccae5983a8060185bf
Pavel <p.a.dudarev@mail.ru>
2022-10-11 05:32:41 +0500

    for

---
+++ Итоговая/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] words = new string[5] {"Привет", "пока","2","кот","бот"};
+List <string> words1 = new List <string> () ;
+for (int i = 0 ; i < words.Length; i++)
+{
+    if(words[i].Length <= 3)
+    {
+        words1.Add (words[i]);
+    }
+}
+
+
+
+
+
